bc8de0a08e2e71ee656520f893af787c7e62298f
AllKeng <allenkeng8@gmail.com>
2024-04-29 20:23:17 -0700

    Added the code I edited/worked on

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,16 @@
 # CHANGELOG
+## 4/29/2024
+### Contributors
+Allen Keng
+
+### Added
+* oldapp.py : code given to us 
+
+### Changed
+* app.py : code I wrote from the wiki + my size changes
+
+### Removed
+...
 
 ## 4/24/2024
 ### Contributors
--- app.py
+++ app.py
@@ -1,87 +1,103 @@
 from __future__ import annotations
-from threading import Thread, Lock
 from serial import Serial, SerialException
 from serial.tools.list_ports import comports
-
+#########################################################################################
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter.messagebox import showerror
-
-S_OK: int = 0xaa
-S_ERR: int = 0xff
+from threading import Thread, Lock # we'll use Lock later ;)
 
 def detached_callback(f):
     return lambda *args, **kwargs: Thread(target=f, args=args, kwargs=kwargs).start()
 
+S_OK: int = 0xaa
+S_ERR: int = 0xff
+
+#with Serial('/your/port', 9600) as ser:
+ #   ser.write(bytes([0x1]))
+  #  print(ser.read() == bytes([0xaa]))
+#
+ #   ser.write(bytes([0x0]))
+  #  print(ser.read() == bytes([0xaa]))
+#
+ #   ser.write(bytes([0x2]))
+  #  print(ser.read() == bytes([0xff]))
+
+############################################################################################
 class LockedSerial(Serial):
     _lock: Lock = Lock()
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
     def read(self, size=1) -> bytes:
         with self._lock:
             return super().read(size)
-        
+
     def write(self, b: bytes, /) -> int | None:
         with self._lock:
             super().write(b)
-            
+
     def close(self):
         with self._lock:
             super().close()
 
+
+#######################################################################################
 class SerialPortal(tk.Toplevel):
     def __init__(self, parent: App):
         super().__init__(parent)
-        
+
+        self.geometry("300x300")
         self.parent = parent
-        self.parent.withdraw()
-        
-        ttk.OptionMenu(self, parent.port, '', *[d.device for d in comports()]).pack()
-        ttk.Button(self, text='Connect', command=self.connect, default='active').pack()
-        
-    @detached_callback
+        self.parent.withdraw() # hide App until connected
+
+        ttk.OptionMenu(self, parent.port, 'Click here to pick a port', *[d.device for d in comports()]).place(relx=.5, rely=0.35, anchor="center")
+        ttk.Button(self, text='Connect', command=self.connect, default='active').place(relx=.5, rely=0.5, anchor="center")
+
     def connect(self):
         self.parent.connect()
         self.destroy()
-        self.parent.deiconify()
+        self.parent.deiconify() # reveal App
 
 class App(tk.Tk):
-    ser: LockedSerial
-    
+    ser: Serial
     def __init__(self):
         super().__init__()
-        
+
         self.title("LED Blinker")
-        
+        self.geometry("300x300")
         self.port = tk.StringVar()
         self.led = tk.BooleanVar()
-        
-        ttk.Checkbutton(self, text='Toggle LED', variable=self.led, command=self.update_led).pack()
-        ttk.Button(self, text='Send Invalid', command=self.send_invalid).pack()
-        ttk.Button(self, text='Disconnect', command=self.disconnect, default='active').pack()
-        
-        SerialPortal(self)
-        
-    @detached_callback
+
+        check1 = ttk.Checkbutton(self, text ='Toggle LED', variable=self.led, command=self.update_led).place(relx=.5, rely=0.35, anchor="center")
+        button1 = ttk.Button(self, text = 'Send invalid', command=self.send_invalid).place(relx=.5, rely=0.5, anchor="center")
+        button2 = ttk.Button(self, text = 'Disconnect', default='active', command=self.disconnect).place(relx=.5, rely=0.65, anchor="center")
+        #check1.pack()
+
+        SerialPortal(self) # and this
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self.disconnect()
+
     def connect(self):
         self.ser = LockedSerial(self.port.get())
-    
-    @detached_callback
-    def disconnect(self):
-        self.ser.close()
-            
-        SerialPortal(self)
-            
+
     @detached_callback
     def update_led(self):
         self.write(bytes([self.led.get()]))
-                
-    @detached_callback
+
+    def disconnect(self):
+        self.ser.close()
+
+        SerialPortal(self) # display portal to reconnect
+
     def send_invalid(self):
-        self.write(bytes([0x10]))
-        
+       self.write(bytes([0x10]))
+
     def write(self, b: bytes):
         try:
             self.ser.write(b)
@@ -89,13 +105,8 @@ class App(tk.Tk):
                 showerror('Device Error', 'The device reported an invalid command.')
         except SerialException:
             showerror('Serial Error', 'Write failed.')
-        
-    def __enter__(self):
-        return self
-    
-    def __exit__(self, *_):
-        self.disconnect()
-        
+
+
 if __name__ == '__main__':
-    with App() as app:
+    with App() as app: 
         app.mainloop()
